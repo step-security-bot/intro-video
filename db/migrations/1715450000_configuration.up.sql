163b3995d1c6c2739e418edc947920349dbcaf56
David Abram <david@crocoder.dev>
2024-05-12 15:03:04 +0200

    update sql

--- db/migrations/1715450000_configuration.up.sql
+++ db/migrations/1715450000_configuration.up.sql
@@ -2,7 +2,9 @@ CREATE TABLE `configurations` (
 	`id` integer PRIMARY KEY NOT NULL,
   `bubble_enabled` boolean DEFAULT false,
   `bubble_text_content` text,
+  `bubble_type` text DEFAULT 'default',
   `cta_enabled` boolean DEFAULT false,
-  `cta_text_content` text
+  `cta_text_content` text,
+  `cta_type` text DEFAULT 'default',
 );
 
