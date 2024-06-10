aa140b5f7bba08bd2052b09d9aaee39f97e04a46
David Abram <david@crocoder.dev>
2024-06-10 23:44:11 +0200

    update video input

--- public/components/video-input.js
+++ public/components/video-input.js
@@ -11,18 +11,18 @@ const validSVG = html`
 
 // indigo-600
 const loaderSVG = html`
-    <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="10" cy="10" r="8" stroke="#4f46e5" stroke-width="2" fill="none" />
-      <circle cx="10" cy="2" r="2" fill="#4f46e5">
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          from="0 10 10"
-          to="360 10 10"
-          dur="1s"
-          repeatCount="indefinite" />
-      </circle>
-    </svg>
+  <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="10" cy="10" r="8" stroke="#4f46e5" stroke-width="2" fill="none" />
+    <circle cx="10" cy="2" r="2" fill="#4f46e5">
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        from="0 10 10"
+        to="360 10 10"
+        dur="1s"
+        repeatCount="indefinite" />
+    </circle>
+  </svg>
 `;
 
 // red-500
@@ -71,9 +71,17 @@ class VideoInput extends LitElement {
     }
   `;
 
+  static properties = {
+    id: { type: String },
+    name: { type: String },
+  }
+
+
   constructor() {
     super();
     this.state = states.initial;
+    this.id = 'video-url';
+    this.name = 'video-url';
   }
 
   handleInput() {
@@ -81,8 +89,8 @@ class VideoInput extends LitElement {
     this.state = states.loading;
     this.requestUpdate();
     this.timeout = setTimeout(async () => {
-      const value = this.shadowRoot.getElementById('video-url').value;
-      if(value !== '') {
+      const value = this.shadowRoot.getElementById(this.id).value;
+      if (value !== '') {
         try {
           await validateVideoUrl(value);
           this.state = states.valid;
@@ -96,20 +104,14 @@ class VideoInput extends LitElement {
 
   render() {
     return html`
-      <label
-        for="video-url"
-        class="block text-sm font-medium leading-6 text-gray-900"
-      >
-        Video URL
-      </label>
       <div class="mt-2">
         <input
-          id="video-url"
-          name="video-url"
+          id="${this.id}"
+          name="${this.name}"
           type="url"
           pattern="https://.*"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            required
+          required
           @input=${this.handleInput}
         />
       ${this.state === states.loading ? loaderSVG : ''}
