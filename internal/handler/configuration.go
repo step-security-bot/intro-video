4595500bb6260c97b3d96d3b97c77abaf00a7ce8
Luka <luka.sesar99@gmail.com>
2024-06-13 13:46:01 +0200

    added form

--- internal/handler/configuration.go
+++ internal/handler/configuration.go
@@ -2,13 +2,44 @@ package handler
 
 import (
 	"context"
+	"net/http"
 
-	"github.com/labstack/echo/v4"
 	"github.com/crocoder-dev/intro-video/internal/template"
+	"github.com/labstack/echo/v4"
 )
 
 func Configuration(c echo.Context) error {
 	component := template.Configuration()
 
 	return component.Render(context.Background(), c.Response().Writer)
+}
+
+func GenerateCode(c echo.Context) error {
+	url := c.FormValue("url")
+	bubbleEnabled := c.FormValue("bubble-enabled")
+	bubbleTextContent := c.FormValue("bubble-text")
+	ctaEnabled := c.FormValue("cta-enabled")
+	ctaTextContent := c.FormValue("cta-text")
+	if url == "" {
+		return c.String(http.StatusBadRequest, "Invalid data")
+	}
+	c.Set("url", url)
+	c.Set("bubbleEnabled", bubbleEnabled)
+	c.Set("bubbleTextContent", bubbleTextContent)
+	c.Set("ctaEnabled", ctaEnabled)
+	c.Set("ctaTextContent", ctaTextContent)
+
+	// err, js := Script(c)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// err, css := Stylesheet(c)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// components := template.CodePreview(css, js)
+	// return components.Render(context.Background(), c.Response().Writer)
+	return nil
 }
